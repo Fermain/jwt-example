961084363d4d8072693f22f0dc1d0e2fa3a72617
Fermain <diplomat@fermain.io>
2022-02-11 10:18:53 +0200

    Updated api auth function

--- js/api/index.js
+++ js/api/index.js
@@ -1,1 +1,31 @@
-export async function auth() {}
+import { baseURL, authPath } from "../constants/api.js";
+
+export async function auth(username, password) {
+  const url = baseURL + authPath;
+
+  const body = {
+    username: username,
+    password: password
+  }
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: JSON.stringify(body)
+    });
+
+    if (response.status !== 200) {
+      throw new Error(response.status);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.warn('User is not allowed to log in', error);
+    alert('Login failed, please check your credentials');
+    return null;
+  }
+}
